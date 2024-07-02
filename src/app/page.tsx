6cc70680c4240d0baba31ea251050ed88bee0f46
Alieu Jagne <ajagne01@gmail.com>
2024-07-02 21:18:05 +0100

    added some new components nut have to fix the nav component 🚨

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { CardBody, CardContainer, CardItem } from "@/components/global/3d-card";
 import { HeroParallax } from "@/components/global/connect-parallax";
 import { ContainerScroll } from "@/components/global/container-scroll-animation";
 import { InfiniteMovingCards } from "@/components/global/inifinite-moving-cards";
+import { LampComponent } from "@/components/global/lamp";
 import Navbar from "@/components/global/navbar";
 import { Button } from "@/components/ui/button";
 import { clients, products } from "@/lib/constant";
+import { CheckIcon } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -5,7 +5,7 @@ import { UserButton, currentUser } from '@clerk/nextjs'
 
 type Props = {};
 
-const Navbar = (props: Props) => {
+const Navbar = async (props: Props) => {
   const user = await currentUser();
   return (
     <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
