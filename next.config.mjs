c7c40b5aaa2291ebdd685454cb4fca5d475b09f9
Alieu Jagne <ajagne01@gmail.com>
2024-07-09 21:29:31 +0100

    added some new component for action & connection but still having issue with the fotbar

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatternes: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "ucarecdn.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
