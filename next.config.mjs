b1a1eb3c02d2807d77bc552b872bf5ced2c912e7
Alieu Jagne <ajagne01@gmail.com>
2024-07-10 00:49:59 +0100

    install sonner and drawer with some other component

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
-    remotePatternes: [
+    remotePatterns: [
       {
         protocol: "https",
         hostname: "img.clerk.com",
